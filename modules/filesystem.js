74bce2580ecd161aabe285b80a9f28d91253be23
Ron Peterson <ron.peterson@byu.edu>
2017-10-05 19:36:02 -0600

    resolves #6: Using different hooks to cleanup files when ready.

--- modules/filesystem.js
+++ modules/filesystem.js
@@ -1,23 +1,2 @@
-var readdir = require('fs').readdir
-
-function waitForFiles( path , callback ){
-    
-    var allFilesPresent
-    var interval
-    interval = setInterval( () => {
-
-        readdir(path , function(err,filesInFolder){
-
-            if(filesInFolder.length >= 4)
-            {
-                clearInterval(interval)
-                callback()
-            }
-        })
-
-    },10)
-}
-
-module.exports.waitForFiles        = waitForFiles
 module.exports.readFile            = require('fs').readFileSync
 module.exports.writeFile           = require('fs').writeFileSync
